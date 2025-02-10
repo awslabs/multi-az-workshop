605a0614d4c781edfd44e0b62af444fbba174877
Michael Haken <mhaken@amazon.com>
2025-02-09 20:05:53 -0500

    fixes

--- app-src/Controllers/HomeController.cs
+++ app-src/Controllers/HomeController.cs
@@ -1,14 +1,15 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-using Amazon.CloudWatch.EMF.Logger;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 using System.Diagnostics;
 using Newtonsoft.Json;
 using Npgsql;
 using Amazon.CloudWatch.EMF.Model;
-using System.Net.Mime; // Don't delete, needed for FAIL
+using System.Net.Mime;
+using BAMCIS.MultiAZApp.Utilities;
+using Amazon.CloudWatch.EMF.Logger; // Don't delete, needed for FAIL
 
 namespace BAMCIS.MultiAZApp.Controllers
 {
@@ -17,16 +18,21 @@ namespace BAMCIS.MultiAZApp.Controllers
     public class HomeController : ControllerBase
     {
         private readonly ILogger<HomeController> logger;
-        private readonly IMetricsLogger metrics;
+        private IMetricsLogger metrics;
         private readonly Random rand;
         private IMemoryCache cache;
+        private IEnvironment env;
 
         private ObjectResult DoWork() {
             Thread.Sleep(this.rand.Next(1, 20));
             this.Response.ContentType = MediaTypeNames.Application.Json;
 
             return Ok(new { 
-                statusCode = 200
+                statusCode = 200,
+                region = env.GetRegion(),
+                azId = env.GetAZId(),
+                instanceId = env.GetInstanceId(),
+                hostId = env.GetHostId()
             });
         }
 
@@ -39,18 +45,23 @@ namespace BAMCIS.MultiAZApp.Controllers
                 type: null, 
                 detail: JsonConvert.SerializeObject(
                     new { 
-                        statusCode = 500
+                        statusCode = 500,
+                        region = env.GetRegion(),
+                        azId = env.GetAZId(),
+                        instanceId = env.GetInstanceId(),
+                        hostId = env.GetHostId()
                     }
                 )
             );
         }
 
-        public HomeController(ILogger<HomeController> logger, IMetricsLogger metrics, IMemoryCache cache)
+        public HomeController(ILogger<HomeController> logger, IMetricsLogger metrics, IMemoryCache cache, IEnvironment env)
         {
             this.logger = logger;
             this.metrics = metrics;
             this.rand = new Random();
             this.cache = cache;
+            this.env = env;
         }
 
         // GET: /health
@@ -117,7 +128,11 @@ namespace BAMCIS.MultiAZApp.Controllers
                     return Ok(
                         new { 
                             statusCode = 200,
-                            tables = content.ToArray() 
+                            tables = content.ToArray(),
+                            region = env.GetRegion(),
+                            azId = env.GetAZId(),
+                            instanceId = env.GetInstanceId(),
+                            hostId = env.GetHostId()
                         }
                     );
                 }
@@ -127,8 +142,12 @@ namespace BAMCIS.MultiAZApp.Controllers
                     return Problem(
                         detail: JsonConvert.SerializeObject(
                             new { 
-                                statusCode = 500, 
-                                exception = e 
+                                statusCode = 500,
+                                exception = e,
+                                region = env.GetRegion(),
+                                azId = env.GetAZId(),
+                                instanceId = env.GetInstanceId(),
+                                hostId = env.GetHostId()
                             }
                         ), statusCode: 500, type: null);
                 }
@@ -139,8 +158,13 @@ namespace BAMCIS.MultiAZApp.Controllers
                 return Problem(
                     detail: JsonConvert.SerializeObject(new { 
                         statusCode = 404, 
-                        problem = "Connection string was empty, check secrets manager configuration." }
-                    ), statusCode: 404, type: null);
+                        problem = "Connection string was empty, check secrets manager configuration.",            
+                        region = env.GetRegion(),
+                        azId = env.GetAZId(),
+                        instanceId = env.GetInstanceId(),
+                        hostId = env.GetHostId()
+                    }
+                ), statusCode: 404, type: null);
             }
         }
     }
