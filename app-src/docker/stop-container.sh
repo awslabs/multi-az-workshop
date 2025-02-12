fae423bcf1f39a2bbd69e7e4169889a1fe0f967b
Michael Haken <mhaken@amazon.com>
2025-02-11 23:10:16 -0500

    removed set -e

--- app-src/docker/stop-container.sh
+++ app-src/docker/stop-container.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+
 rootDirectory="/opt/codedeploy-agent/deployment-root"
 compose="$rootDirectory/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/docker/docker-compose.yml"
 docker compose --file $compose down || true
