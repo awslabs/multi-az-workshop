ebaaa4fdc341a41a81a1b91db6be1a3db0bdfef1
Michael Haken <mhaken@amazon.com>
2025-02-11 22:46:10 -0500

    fixed syntax

--- app-src/docker/start-container.sh
+++ app-src/docker/start-container.sh
@@ -2,4 +2,4 @@
 
 rootDirectory="/opt/codedeploy-agent/deployment-root"
 compose="$rootDirectory/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/docker/docker-compose.yml"
-docker compose --detach -f $compose up
+docker compose --file $compose up --detach
--- app-src/docker/stop-container.sh
+++ app-src/docker/stop-container.sh
@@ -2,4 +2,4 @@
 
 rootDirectory="/opt/codedeploy-agent/deployment-root"
 compose="$rootDirectory/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/docker/docker-compose.yml"
-docker compose -f $compose down
+docker compose --file $compose down
