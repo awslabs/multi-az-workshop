805e2e26d20a38d7ce8107fbde049e5fe3f6e867
Michael Haken <mhaken@amazon.com>
2025-02-11 22:10:03 -0500

    updated scripts

--- app-src/docker/remove-container.sh
+++ app-src/docker/remove-container.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-set -e
 rootDirectory="/opt/codedeploy-agent/deployment-root"
 compose="$rootDirectory/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/docker/docker-compose.yml"
 
-docker compose rm --file $compose --force --stop --volumes || true
+docker compose rm -f $compose --force --stop --volumes
--- app-src/docker/stop-container.sh
+++ app-src/docker/stop-container.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-set -e
 rootDirectory="/opt/codedeploy-agent/deployment-root"
 compose="$rootDirectory/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/docker/docker-compose.yml"
-docker compose down --file $compose || true
+docker compose down -f $compose
