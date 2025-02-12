b27f4e34081fb9625342c48cad17e28a15fcd899
Michael Haken <mhaken@amazon.com>
2025-02-12 09:04:12 -0500

    setting db secret env var

--- app-src/docker/start-container.sh
+++ app-src/docker/start-container.sh
@@ -2,4 +2,5 @@
 
 rootDirectory="/opt/codedeploy-agent/deployment-root"
 compose="$rootDirectory/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/docker/docker-compose.yml"
-docker compose --file $compose up --detach
+DB_SECRET=$(cat /etc/secret)
+docker compose --file $compose up --detach --env DB_SECRET=$DB_SECRET
