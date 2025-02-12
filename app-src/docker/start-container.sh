133004defcc9ac662325521da931d639f0e12412
Michael Haken <mhaken@amazon.com>
2025-02-12 10:32:33 -0500

    adding env file

--- app-src/docker/start-container.sh
+++ app-src/docker/start-container.sh
@@ -2,5 +2,7 @@
 
 rootDirectory="/opt/codedeploy-agent/deployment-root"
 compose="$rootDirectory/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/docker/docker-compose.yml"
+env="$rootDirectory/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/docker/.env"
 DB_SECRET=$(cat /etc/secret)
-docker compose --file $compose up --detach
+echo "DB_SECRET=$DB_SECRET" >> $env
+docker compose --file $compose --env-file $env up --detach
