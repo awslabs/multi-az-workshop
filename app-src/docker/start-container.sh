b84e05916017bf6be7589542f6f34a58cbc8ef17
Michael Haken <mhaken@amazon.com>
2025-02-12 10:01:46 -0500

    updating env var

--- app-src/docker/start-container.sh
+++ app-src/docker/start-container.sh
@@ -3,4 +3,4 @@
 rootDirectory="/opt/codedeploy-agent/deployment-root"
 compose="$rootDirectory/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/docker/docker-compose.yml"
 DB_SECRET=$(cat /etc/secret)
-docker compose --file $compose up --detach --env DB_SECRET=$DB_SECRET
+docker compose --file $compose up --detach
