f80bb63f88803b2155d0f5e9f118c81c10c59f28
Michael Haken <mhaken@amazon.com>
2025-02-13 16:09:28 -0500

    removing ec2

--- cdk/NestedStacks/EC2FleetStack.cs
+++ cdk/NestedStacks/EC2FleetStack.cs
@@ -490,7 +490,6 @@ namespace Amazon.AWSLabs.MultiAZWorkshop.NestedStacks
                 {
                     "04_install-cloudwatch-agent",
                     new InitConfig(new InitElement[] {
-                    //    InitCommand.ShellCommand(Fn.Sub("rpm --upgrade --force -v -h  https://s3.${AWS::Region}.${AWS::URLSuffix}/amazoncloudwatch-agent-${AWS::Region}/amazon_linux/${arch}/latest/amazon-cloudwatch-agent.rpm", new Dictionary<string, string>(){ { "arch", props.CpuArch == InstanceArchitecture.ARM_64 ? "arm64" : "amd64" } }))
                         InitPackage.Yum("amazon-cloudwatch-agent"),
                         //InitCommand.ShellCommand("/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a set-log-level -l DEBUG"),
                     })
@@ -556,8 +555,6 @@ namespace Amazon.AWSLabs.MultiAZWorkshop.NestedStacks
                             InitCommand.ShellCommand("mkdir -p /usr/libexec/docker/cli-plugins"),
                             InitCommand.ShellCommand("aws s3 cp s3://" + props.AssetsBucketName + "/" + props.AssetsBucketPrefix + "docker-compose /usr/libexec/docker/cli-plugins/docker-compose --region " + Aws.REGION),
                             InitCommand.ShellCommand("chmod +x /usr/libexec/docker/cli-plugins/docker-compose"),
-                            //InitCommand.ShellCommand("systemctl enable docker"),
-                            //InitCommand.ShellCommand("systemctl start docker")
                             InitService.Enable("docker", new InitServiceOptions() {
                                 Enabled = true,
                                 EnsureRunning = true,
