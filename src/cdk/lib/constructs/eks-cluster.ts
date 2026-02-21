d5e56df981f52ca32c1d0ba1c37076661572a3b7
Michael Haken <michael.haken@outlook.com>
2026-02-20 21:29:30 -0500

    fix: trying v35

--- src/cdk/lib/constructs/eks-cluster.ts
+++ src/cdk/lib/constructs/eks-cluster.ts
@@ -1,7 +1,8 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { KubectlV34Layer as KubectlLayer } from '@aws-cdk/lambda-layer-kubectl-v34';
+//import { KubectlV34Layer as KubectlLayer } from '@aws-cdk/lambda-layer-kubectl-v34';
+import { KubectlV35Layer as KubectlLayer } from '@aws-cdk/lambda-layer-kubectl-v35';
 import * as cdk from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as eks from 'aws-cdk-lib/aws-eks';
