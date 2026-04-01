9723e7957024cd317fc0aa5f2d53a55525108c37
Michael Haken <michael.haken@outlook.com>
2026-04-01 13:38:03 -0700

    fix: linting

--- .projenrc.ts
+++ .projenrc.ts
@@ -74,7 +74,7 @@ const project = new AwsCdkTypeScriptApp({
     '@types/node',
     '@cdklabs/multi-az-observability@0.0.1-alpha.60',
     '@aws-cdk/lambda-layer-kubectl-v35@^2.0.0',
-    'fast-check@^3.15.0'
+    'fast-check@^3.15.0',
   ],
   deps: [],
   peerDeps: [],
